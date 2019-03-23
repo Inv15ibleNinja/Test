ca7924b5dc67061096fd0cdaa58276d173bc5b64
Alexandr Gorodilov <gorodilov.alex@gmail.com>
2019-03-23 22:34:48 +0300

    fix

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Test
+{
+    class FileMover
+    {
+        string sourcePath, destinationPath;
+
+        static void Main(string[] args)
+        {
+            FileMover fm1 = new FileMover();
+            fm1.sourcePath = @"C:\";
+            fm1.destinationPath = @"D:\";
+            Console.WriteLine("Добро пожаловать!\nДля копирования файлов введите путь к исходному и конечному каталогу и выберите необходимые параметры.");
+            Console.WriteLine("Путь к исходной папке: ");
+            fm1.sourcePath += Console.Read();
+            Console.WriteLine("Путь к конечной папке: ");
+            fm1.destinationPath += Console.Read();
+            //Console.Clear();
+            Console.WriteLine("Выберите параметры копирования: ");
+
+
+            DirectoryInfo dirInfo = new DirectoryInfo(fm1.sourcePath);
+
+            foreach (FileInfo file in dirInfo.GetFiles("*.*"))
+            {
+                try
+                {
+                    File.Copy(file.FullName, fm1.destinationPath + "\\" + file.Name, true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибочка вышла" + ex.Message);
+                }
+            }
+
+
+            //f.sourcePath="";
+            /*try{
+                File.Copy(f.sourcePath, f.destinationPath, true);
+            }
+            catch (IOException copyError){
+                Console.WriteLine(copyError.Message);
+            }*/
+
+        }
+    }
+}
+
