cbbdbe50b5e9532746d529fdf261485aa47be1f8
Alexandr Gorodilov <gorodilov.alex@gmail.com>
2019-03-28 20:02:30 +0300

    вложенные папки, удаление, вывод имен

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,43 +5,53 @@ using System.Diagnostics;
 using System.IO;
 using System.Globalization;
 using System.Threading;
+//TODO: таймер, потоки
 
 namespace Test
 {
     class FileMover
     {
-        string sourcePath, destinationPath;
-        double interval;
+        string sourcePath= @"C:\", destinationPath= @"D:\";
+        int interval;
+        bool delete=false, showNames=false,copySubDir=false; 
 
         static void Main(string[] args)
         {
             FileMover fm1 = new FileMover();//создаём копир
-            fm1.sourcePath = @"C:\";//папки по дефолту
-            fm1.destinationPath = @"D:\";
-            Console.WriteLine("Добро пожаловать!\nДля копирования файлов введите путь к исходному и конечному каталогу и выберите необходимые параметры.");
+            //fm1.sourcePath ;//папки по дефолту
+            //fm1.destinationPath ";
+            
+            Console.WriteLine("Добро пожаловать!\nДля копирования файлов введите путь" +
+                " к исходному и конечному каталогу и выберите необходимые параметры.");
             Console.WriteLine("Путь к исходной папке: ");
             fm1.sourcePath = string.Copy(Console.ReadLine());
             Console.WriteLine("Путь к конечной папке: ");
             fm1.destinationPath = string.Copy(Console.ReadLine());
-            Console.WriteLine("Введите интервал в секундах:");
+            Console.WriteLine("Введите интервал в секундах:");            
             fm1.interval = int.Parse(Console.ReadLine());
-            Console.WriteLine("Копируем из "+fm1.sourcePath + " в " + fm1.destinationPath+" втечение "+fm1.interval+" секунд");
+            Console.WriteLine("Если нужно выводить имена файлов при копировнии, введите 1, иначе 0");
+            if (int.Parse(Console.ReadLine()) == 1) fm1.showNames = true;
+            else fm1.showNames = false;
+            Console.WriteLine("Если нужно удалять файлы из исходной папки, после копирования, введите 1, иначе 0");
+            if (int.Parse(Console.ReadLine()) == 1) fm1.delete = true;
+            else fm1.delete = false;
+            Console.WriteLine("Если нужно копировать вложеные папки и файлы в них, после копирования, введите 1, иначе 0");
+            if (int.Parse(Console.ReadLine()) == 1) fm1.copySubDir = true;
+            else fm1.copySubDir = false;
+
+
 
             DirectoryInfo diSource = new DirectoryInfo(fm1.sourcePath);
             DirectoryInfo diTarget = new DirectoryInfo(fm1.destinationPath);
 
 
-
-            DateTime finish = DateTime.Now.AddSeconds(fm1.interval);
-            ;
-            Console.WriteLine("Готово. скопировано "+ Copy(diSource, diTarget,finish, 0)+ "файлов");
+            
+            
+            Console.WriteLine("Готово. скопировано " + Copy(diSource, diTarget, fm1.interval, 0,fm1.delete, fm1.showNames,fm1.copySubDir) + " файлов");
             Console.WriteLine("выход");
         }
-        public static int Copy(DirectoryInfo source, DirectoryInfo target, DateTime stop, int count)
+        public static int Copy(DirectoryInfo source, DirectoryInfo target, int interval, int count,bool delete, bool showNames, bool copySubDir)
         {
-
-   
-
 
                 if (source.FullName.ToLower() == target.FullName.ToLower())//проверяем папки
                 {
@@ -57,29 +67,31 @@ namespace Test
 
                 foreach (FileInfo fi in source.GetFiles())//копируем файлы
                 {
-                if (DateTime.Now < stop)
-                {
+                    
 
-                    Console.WriteLine(@"Копирую {0}\{1}", target.FullName, fi.Name);
-                    fi.CopyTo(Path.Combine(target.ToString(), fi.Name), true);
+                        if(showNames)Console.WriteLine(@"Копирую {0}\{1}", target.FullName, fi.Name);
+                        fi.CopyTo(Path.Combine(target.ToString(), fi.Name), true);
+                        count++;
+                        if (delete) fi.Delete();
+                        Console.WriteLine("Скопировано " + count + " файлов");
+                if(Console.ReadLine()!="Stop")Thread.Sleep(interval);
+            }
+
+
+            if (copySubDir) foreach (DirectoryInfo diSourceSubDir in source.GetDirectories()) //копируем вложенные папки
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                Copy(diSourceSubDir, nextTargetSubDir, interval, count,delete,showNames,copySubDir);
+                    if(delete) diSourceSubDir.Delete();
                     count++;
-                    Console.WriteLine("Скопировано " + count + " файлов");
-                }
-                else return count;
 
-                }
-
-                foreach (DirectoryInfo diSourceSubDir in source.GetDirectories()) //копируем вложенные папки
-                {
-                    DirectoryInfo nextTargetSubDir =
-                        target.CreateSubdirectory(diSourceSubDir.Name);
-                    Copy(diSourceSubDir, nextTargetSubDir, stop, count);
                 }
             return count;
                        
             
         }
-
+        
         
     }
 }
